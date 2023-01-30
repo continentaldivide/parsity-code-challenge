43765ffbc51b1fa4d009df30b4b04ec7a57f9235
Andrew <andrew@andrewsmith.org>
2023-01-30 18:43:24 -0500

    Add problem 2 solution

--- submission.js
+++ submission.js
@@ -3,7 +3,29 @@ const findSum = function (array) {
 };
 
 const findFrequency = function (array) {
-  // your code here - don't forget to return an object!
+  let most = 0;
+  let mostID = "";
+  let least = 999;
+  let leastID = "";
+  const countElement = (element) => {
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == element) {
+        total++;
+      }
+    }
+    if (total > most) {
+      most = total;
+      mostID = element;
+    }
+    if (total < least) {
+      least = total;
+      leastID = element;
+    }
+  };
+  array.forEach((element) => countElement(element));
+  const answer = { most: mostID, least: leastID };
+  return answer;
 };
 
 const isPalindrome = function (str) {
